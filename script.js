48a2d295cb8492746cf977ffad1075ef1be8ffd6
georgivishnya <georgi.vishnakov@tumo.org>
2023-10-27 17:36:14 +0400

    proect

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let colorObj = {
+    green : "green",
+    yellow: "yellow",
+    purple: "purple",
+    black: "black",
+    red: "red"
+}
+
 let socket = io();
 
 var side = 70;
@@ -7,29 +15,29 @@ function setup() {
     background('#acacac');
 }
 
-function drawful( matrix ) {
+function drawful(matrix) {
 
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
 
             if (matrix[y][x] == 1) {
-                fill("green");
+                fill(colorObj.green);
             }
             else if (matrix[y][x] == 0) {
                 fill("#acacac");
             }
             else if (matrix[y][x] == 2) {
-                fill("yellow")
+                fill(colorObj.yellow)
             }
             else if (matrix[y][x] == 3) {
-                fill("purple")
+                fill(colorObj.purple)
             }
 
             else if (matrix[y][x] == 4) {
-                fill("black")
+                fill(colorObj.black)
             }
             else if (matrix[y][x] == 5) {
-                fill("red")
+                fill(colorObj.red)
             }
             rect(x * side, y * side, side, side);
 
@@ -40,3 +48,49 @@ function drawful( matrix ) {
     }
 }
 socket.on('matrix', drawful)
+
+
+
+
+let spring = document.getElementById("spr")
+let summer = document.getElementById("sum")
+let winter = document.getElementById("wt")
+let autmn = document.getElementById("at")
+
+spring.addEventListener("click", season)
+summer.addEventListener("click", season)
+autmn.addEventListener("click", season)
+winter.addEventListener("click", season)
+
+
+
+function season() {
+    if( spring.innerText === "Spring" ) {
+        colorObj.green = "orange"
+        colorObj.yellow = "blue"
+        colorObj.purple = "brown"
+        colorObj.black = "gray"
+        colorObj.red = "violet"        
+    } else if (winter.innerText === "Winter") {
+        colorObj.green = "white"
+        colorObj.yellow = "gold"
+        colorObj.purple = "khaki"
+        colorObj.black = "indigo"
+        colorObj.red = "lavender"     
+    } else if (summer.innerText === "Summer") {
+        colorObj.green = "ivory"
+        colorObj.yellow = "lime"
+        colorObj.purple = "magenta"
+        colorObj.black = "maroon"
+        colorObj.red = "navy"     
+    } else if (autmn.innerText === "Autmn") {
+        colorObj.green = "olive"
+        colorObj.yellow = "orchid"
+        colorObj.purple = "plum"
+        colorObj.black = "peru"
+        colorObj.red = "pink"     
+    }
+
+}
+
+
---
+++ water.js
@@ -0,0 +1,5 @@
+// class Water
+// constructor(x, y, index) {
+//     this.energy = 10;
+//     this.multiply = 0;
+// }
